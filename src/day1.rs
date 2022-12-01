85386114da61d0537b300fcf720021075c317845
Ujjwal <epicujjwal@protonmail.com>
2022-12-01 09:02:58 +0000

    part2 day 1 complete

--- src/day1.rs
+++ src/day1.rs
@@ -1,13 +1,12 @@
 use std::fs::File;
 use std::io::Read;
-use std::io;
 
 pub fn solve() {
     let path = "./day1/input.txt";
     let mut file: File = File::open(path).unwrap();
         let mut contents: String = String::new();
         file.read_to_string(&mut contents).unwrap();
-        let mut a: Vec<&str> = contents.split("\n\n").collect();
+        let a: Vec<&str> = contents.split("\n\n").collect();
         let mut totals: Vec<u32> = vec![];
         for e in a {
             let mut total_cal = 0u32;
@@ -18,9 +17,19 @@ pub fn solve() {
             }
             totals.push(total_cal);
         }
-        let max = totals.iter().max().unwrap();
-        println!("MAX: {}", max);
-
-        
+        let mut max = totals.iter().max().cloned().unwrap();
+        println!("{}", max);
+        let mut t3_vec: Vec<u32> = vec![];
+        for _ in 0..3 { 
+            t3_vec.push(max);
+            let idx = totals.clone().iter().position(|&x| x == max).unwrap();
+            totals.remove(idx);
+            max = totals.iter().max().unwrap().clone();
+        }
+        let mut t3_sum = 0u32;
+        for i in t3_vec {
+            t3_sum = i + t3_sum
+        }
+        println!("{t3_sum}");
 
 }
